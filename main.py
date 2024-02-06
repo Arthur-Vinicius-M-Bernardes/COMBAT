f4767516d77cae63c5a15948d60ae3c2bd8bc549
José Victor Matos de Sousa <150152517+Jose-victor-matos-de-sousa@users.noreply.github.com>
2024-02-06 09:21:15 -0400

    Create main.py
    organizar depois

---
+++ main.py
@@ -0,0 +1,22 @@
+import tank
+import map
+import bullet
+
+
+print("comandos de movimento: W, A, S, D")
+option=input("comando de disparo: F")
+match option:
+    case "W":
+        tank.acoes
+
+    case "A":
+        tank.acoes
+
+    case "S":
+        tank.acoes
+
+    case "D":
+        tank.acoes
+        
+    case "F":
+        
